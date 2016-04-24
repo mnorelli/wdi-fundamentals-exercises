10e419778fda5a7144c70671ba71b6a8403c77d7
mnorelli <mnorelli360@gmail.com>
2016-04-24 11:14:04 -0700

    first try at 5.2 solution

---
+++ 5.2.1.js
@@ -0,0 +1,10 @@
+var result;
+var max = 15;
+/* YOUR CODE HERE */
+for (x=1;x<=max;x+=1) {
+    if (((x%3)===0)&&((x%5)===0)) {var result = 'fizzbuzz';}
+    else if ((x%3)===0) {var result = 'fizz';}
+    else if ((x%5)===0) {var result = 'buzz';}
+    else var result = x;
+    console.log(result);
+}
