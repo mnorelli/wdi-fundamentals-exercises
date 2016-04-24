3d792603c76ac984c6692803777489141010726a
mnorelli <mnorelli360@gmail.com>
2016-04-23 19:12:45 -0700

    adds five functions

---
+++ 5.1.1.js
@@ -0,0 +1,20 @@
+function sayHello(name) {
+  return "Hello, "+name;
+}
+function areBothEven(a,b) {
+  if ((a%2===0)&&(b%2===0)) {return true;}
+  else return false;
+}
+function hotOrNot(temp) {
+  if (temp > 75) {return "hot";}
+  else return "not hot";
+}
+function threeIfNull(num) {
+  if (num===null) {return 3;}
+  else return num;
+}
+function greatest(x,y,z) {
+  if ((x>y)&&(x>z)) {return x;}
+  else if ((y>x)&&(y>z)) {return y;}
+  else if ((z>x)&&(z>y)) {return z;}
+}
